9d2bdf925e762812eee693850249cff0db4dc9bb
Dominic Philip <domi.a.philip@gmail.com>
2019-06-05 17:10:23 -0400

    add audio analysis and audio features availability

--- beats_brain.py
+++ beats_brain.py
@@ -1,7 +1,3 @@
-import requests
-
-from pprint import pprint
-
 from lib.data import auth
 from lib.data import search
 
@@ -10,7 +6,23 @@ def main():
     token = auth.get_token()
     query = input('Please enter a track name: ')
 
-    results = search.search_track(token, query)
+    search_results = search.search_track(token, query)
+
+    index = 1
+    for item in search_results['tracks']['items']:
+        print(str(index) + ": " + item['name'] + " - " + item['album']['artists'][0]['name'])
+        index += 1
+
+    print()
+
+    track_index = int(input('Please enter the number to a track: '))
+    while track_index not in range(1, index):
+        track_index = int(input('Please enter the number to a track: '))
+
+    track_id = search_results['tracks']['items'][track_index - 1]['id']
+
+    track_analysis = search.search_track_analysis(token, track_id)
+    track_features = search.search_track_features(token, track_id)
 
 
 if __name__ == '__main__':
--- lib/data/search.py
+++ lib/data/search.py
@@ -11,3 +11,21 @@ def search_track(token, query, limit=10):
 
         response = requests.get(url=url, params=payload, headers=headers)
         return response.json()
+
+
+def search_track_analysis(token, track_id):
+    if token:
+        headers = {'Authorization': "Bearer " + token}
+        url = "https://api.spotify.com/v1/audio-analysis/" + track_id
+
+        response = requests.get(url=url, headers=headers)
+        return response.json()
+
+
+def search_track_features(token, track_id):
+    if token:
+        headers = {'Authorization': "Bearer " + token}
+        url = "https://api.spotify.com/v1/audio-features/" + track_id
+
+        response = requests.get(url=url, headers=headers)
+        return response.json()
