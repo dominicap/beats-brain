c362fe74c968d3bfab4738868e990982b201cd82
Dominic Philip <domi.a.philip@gmail.com>
2019-06-05 19:56:24 -0400

    forward to backslash

--- beats_brain.py
+++ beats_brain.py
@@ -10,7 +10,7 @@ def main():
 
     index = 1
     for item in search_results['tracks']['items']:
-        print(str(index) + ": " + item['name'] + /
+        print(str(index) + ": " + item['name'] + \
               " - " + item['album']['artists'][0]['name'])
         index += 1
 
