f6cfa0157cab31ae3ecad9ab3cb0c79d94126ec6
Dominic Philip <domi.a.philip@gmail.com>
2019-06-09 00:03:54 -0400

    add hierarchical clustering, with predetermined clusters

--- lib/predict/cluster.py
+++ lib/predict/cluster.py
@@ -4,14 +4,35 @@ import pandas as pd
 
 import sqlite3
 
+from sklearn import preprocessing
 from sklearn.cluster import AgglomerativeClustering
 
 
 def main():
-    dataframe = dataframe_from_database('db/billboard-200.db', 'acoustic_features')
+    dataframe = dataframe_from_database('db/billboard-200.db',
+                                        'acoustic_features')
+    dataframe.index = dataframe['song'] + ' ' + dataframe['artist']
+
     dataframe = dataframe.dropna()
 
-    print(len(dataframe))
+    columns = ['id', 'song', 'album', 'artist', 'album_id', 'date']
+    dataframe = dataframe.drop(columns=columns)
+
+    values = dataframe.values
+    scaler = preprocessing.MinMaxScaler()
+    values = scaler.fit_transform(values)
+
+    dataframe = pd.DataFrame(values, columns=dataframe.columns, index=dataframe.index)
+
+    sample = dataframe.sample(frac=0.01)
+
+    cluster = AgglomerativeClustering(n_clusters=(int(0.01 * len(dataframe) * 0.5)))
+    cluster.fit(sample)
+
+    sample['labels'] = cluster.labels_
+
+    sample = sample.sort_values(by=['labels'])
+    sample.to_csv('sample.csv')
 
 
 def dataframe_from_database(database, table):
